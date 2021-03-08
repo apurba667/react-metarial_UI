94f5b3b969833de2086c45bbe683efd1432143e1
APURBA ROY <apurbaanikroy@gmail.com>
2021-03-08 17:45:46 +0600

    ...

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import {Button} from '@material-ui/core';
 import './App.css';
 import { formatMs } from '@material-ui/core';
 import News from './Components/News/News';
+import axios from 'axios';
 
 function App() {
   const [articles,setArticles] = useState([]);
+  // useEffect(()=>{
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=ea1f9f52f6fe43a1b73b88490ba7d677'
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data=>setArticles(data.articles))
+  // },[])
   useEffect(()=>{
-    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=ea1f9f52f6fe43a1b73b88490ba7d677'
-    fetch(url)
-    .then(res => res.json())
-    .then(data=>setArticles(data.articles))
+    const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=ea1f9f52f6fe43a1b73b88490ba7d677"
+    axios(url)
+    .then(data => setArticles(data.data.articles));
   },[])
   return (
     <div >
